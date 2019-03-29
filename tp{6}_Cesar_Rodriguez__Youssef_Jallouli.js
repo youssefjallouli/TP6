028c9ca837bda6194aab8de1e3cbe3b9ec2217d8
youssefjallouli <46608542+youssefjallouli@users.noreply.github.com>
2019-03-29 18:34:44 -0400

    Update tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js

--- tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
+++ tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
@@ -1,2 +1,20 @@
 // coéquipier # 1: César Rodriguez
 // coéquipier # 2: Youssef Jallouli
+/*
+*
+*/
+var creerHistoire = function (n) {
+var i = 1 ; 
+var phrase =prompt("Entrez une phrase!") ;
+
+while(i<=n){
+    i++;
+
+
+
+
+
+}
+};
+var histoire = creerHistoire(/* n */);
+print(histoire);
