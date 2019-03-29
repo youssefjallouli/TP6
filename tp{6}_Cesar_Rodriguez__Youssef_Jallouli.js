74981a7ca55b0b1bae01b48ddf58476afef7cb98
Aerosiderolito <cesaraumon@hotmail.com>
2019-03-29 19:24:38 -0400

    Update tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js

--- tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
+++ tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
@@ -9,12 +9,18 @@
 
 var creerHistoire = function (n) {
 
+    var i= 1;
     var phrase =prompt("Entrez une phrase!") ;
     var tab1 = phrase.split(' ');
-    
 
-    print(tab1);
+    var Avant_A_derniere= tab1[tab1.length-3];
+    var Avant_derniere= tab1[tab1.length-3];
+    var Dernière= tab1[tab1.length-3];
+
+    var three_last_words = Avant_A_derniere+" "+Avant_derniere+" "+ Dernière;
+
+    //print(AAdernière+ " " +Aderniere+ " " +dernière);
 };
-
+/* */
 var histoire = creerHistoire(/* n */);
     print(histoire);
