c41008334209d36dc0d1b9c420ec553765fb97f9
Aerosiderolito <cesaraumon@hotmail.com>
2019-03-29 18:53:27 -0400

    Update tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js

--- tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
+++ tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
@@ -1,2 +1,28 @@
 // coéquipier # 1: César Rodriguez
 // coéquipier # 2: Youssef Jallouli
+/**
+ * Cette fonction permet à l'utilisateur
+ * d'initialiser une histoire
+ * 
+ * @param {string} n: mot transmise par l'utilisateur 
+ */
+
+var creerHistoire = function (n) {
+    var i = 1 ; 
+    var phrase =prompt("Entrez une phrase!") ;
+    var tab1 = [];
+
+    
+    for (var i; i<=0; i++){
+        if (phrase.charAt(i)!=""){
+
+            
+
+        }
+
+
+    }
+};
+
+var histoire = creerHistoire(/* n */);
+    print(histoire);
