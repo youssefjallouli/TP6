e341c622017710b36b48a142583ca547afbfd6a0
youssefjallouli <46608542+youssefjallouli@users.noreply.github.com>
2019-03-29 20:00:15 -0400

    Update tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js

--- tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
+++ tp{6}_Cesar_Rodriguez__Youssef_Jallouli.js
@@ -8,19 +8,29 @@
  */
 
 var creerHistoire = function (n) {
+       var i =1 ;
 
-    var i= 1;
     var phrase =prompt("Entrez une phrase!") ;
     var tab1 = phrase.split(' ');
-
     var Avant_A_derniere= tab1[tab1.length-3];
-    var Avant_derniere= tab1[tab1.length-3];
-    var Dernière= tab1[tab1.length-3];
-
+    var Avant_derniere= tab1[tab1.length-2];
+    var Dernière= tab1[tab1.length-1];
+    
     var three_last_words = Avant_A_derniere+" "+Avant_derniere+" "+ Dernière;
+    while (i<=n) {
+        var nvPhrase=prompt(three_last_words);
+        var tab2=nvPhrase.split(" ");
+        tab1=tab1.concat(tab2) ;print(tab1) ;
+        var Avant_A_derniere= tab1[tab1.length-3];
+        var Avant_derniere= tab1[tab1.length-2];
+        var Dernière= tab1[tab1.length-1];
+        var three_last_words = Avant_A_derniere+" "+Avant_derniere+" "+ Dernière;
+       if(i<n) confirm("Passez le contrôle à l'autre joueur!");
+    };
+    return phrase.join(" ");
 
-    //print(AAdernière+ " " +Aderniere+ " " +dernière);
+    
 };
-/* */
-var histoire = creerHistoire(/* n */);
+var n =prompt("Vous etes combien de joueurs SVP ? ") ;
+var histoire = creerHistoire(n);
     print(histoire);
